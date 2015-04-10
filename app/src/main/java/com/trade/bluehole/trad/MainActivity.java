d0e4b49847368cb14d9f715f32f18d58d95b7569
zheng <zheng@gmail.com>
2015-04-11 01:13:56 +0800

    超级主页

--- app/src/main/java/com/trade/bluehole/trad/MainActivity.java
+++ app/src/main/java/com/trade/bluehole/trad/MainActivity.java
@@ -87,6 +87,13 @@ public class MainActivity extends ActionBarActivity {
         UserInfoActivity_.intent(this).start();
     }
 
+    @Click(R.id.shopMainButton)
+    void shopMainButton(){
+        Intent intent=new Intent(this,SuperMainActivity.class);
+        startActivity(intent);
+        //UserInfoActivity_.intent(this).start();
+    }
+
     @Click(R.id.addProductButton)
     void addProduct(){
         NewProductActivity_.intent(this).start();
---
+++ app/src/main/java/com/trade/bluehole/trad/SuperMainActivity.java
@@ -0,0 +1,84 @@
+package com.trade.bluehole.trad;
+
+import android.app.ActionBar;
+import android.app.Activity;
+
+import android.content.res.Configuration;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.LinearLayout;
+
+import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
+import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
+
+public class SuperMainActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerArrowDrawable drawerArrow;
+    private boolean drawerArrowColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_main);
+        ActionBar ab = getActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (LinearLayout) findViewById(R.id.navdrawer);
+
+
+        drawerArrow = new DrawerArrowDrawable(this) {
+            @Override
+            public boolean isLayoutRtl() {
+                return false;
+            }
+        };
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, drawerArrow, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                mDrawerLayout.closeDrawer(mDrawerList);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
